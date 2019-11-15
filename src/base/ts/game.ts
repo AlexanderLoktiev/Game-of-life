e00865122e8694fddfd856c4c6bb76ffd9ef3d23
al <alexander.loktev.stratis@gmail.com>
2019-11-15 18:54:55 +0200

    Implemented "Draw" method for the game.

--- src/base/ts/game.ts
+++ src/base/ts/game.ts
@@ -4,18 +4,19 @@ export default class Game {
     public canvasWidth: any;
     public canvasHeight: any;
     public options: any;
+    public squaresInRow: number | null;
+    public squaresInColumn: number | null;
+    private matrix: any[] = [];
 
     constructor(canvasId = 'scene',
                 options = {
-                    squareSize: 20
+                    squareSize: 10
                 }
     ) {
         this.canvas = document.querySelector(`#${canvasId}`);
         this.options = options;
 
-        this.getContext();
-        this.getCanvasDimensions();
-        this.gridDraw();
+        this.init();
     }
 
     getContext() {
@@ -34,7 +35,6 @@ export default class Game {
             this.ctx.moveTo(x, 0);
             this.ctx.lineTo(x, this.canvasHeight);
         }
-        console.log(this.canvasHeight);
 
         for (var y = 0; y < this.canvasWidth; y += this.options.squareSize) {
             this.ctx.moveTo(0, y);
@@ -43,5 +43,53 @@ export default class Game {
 
         this.ctx.strokeStyle = "#888";
         this.ctx.stroke();
+    }
+
+    getQuantitySquaresInRow() {
+        this.squaresInRow = this.canvasWidth / this.options.squareSize;
+    }
+
+    getQuantitySquaresInColumn() {
+        this.squaresInColumn = this.canvasHeight / this.options.squareSize;
+    }
+
+    initMatrix() {
+        for (let i = 0; i < this.squaresInRow; i++) {
+            this.matrix[i] = [];
+            for (let j = 0; j < this.squaresInColumn; j++) {
+                this.matrix[i][j] = Math.random() < 0.9 ? 0 : 1;
+            }
+        }
+    }
+
+    paintScene() {
+        this.matrix.forEach((row, rowKey) => {
+            row.forEach((column, columnKey) => {
+                column !== 0 ? (
+                    this.ctx.fillStyle = '#00fe39',
+                    this.ctx.fillRect(rowKey * this.options.squareSize, columnKey * this.options.squareSize, this.options.squareSize, this.options.squareSize)
+                ) : null;
+            });
+        });
+    }
+
+    initEvents() {
+        if (this.canvas) {
+            this.canvas.addEventListener('click', e => {
+                console.log(e);
+            });
+        }
+    }
+
+    init() {
+        this.getContext();
+        this.getCanvasDimensions();
+        this.gridDraw();
+        this.getQuantitySquaresInRow();
+        this.getQuantitySquaresInColumn();
+        this.initMatrix();
+        this.paintScene();
+        this.initEvents();
+
     }
 }
